6717b8be093c84e867768c612659156c4208b909
Maria <s185108@student.dtu.dk>
2024-06-21 15:07:35 +0200

    Implemented submenus in ProfileFragment with each their own Fragment

--- app/src/main/java/com/example/mentalitree/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/mentalitree/ui/profile/ProfileFragment.java
@@ -5,14 +5,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.mentalitree.R;
 import com.example.mentalitree.databinding.FragmentProfileBinding;
+import com.example.mentalitree.ui.profile.submenus.notes.NotesFragment;
+import com.example.mentalitree.ui.profile.submenus.settings.SettingsFragment;
+import com.example.mentalitree.ui.profile.submenus.statistics.StatisticsFragment;
+import com.example.mentalitree.ui.profile.submenus.workbook.WorkbookFragment;
 
 import java.util.ArrayList;
 
@@ -38,8 +42,17 @@ public class ProfileFragment extends Fragment implements View.OnClickListener {
         statisticsBtn = binding.statisticsBtn;
         statisticsBtn.setOnClickListener(this);
 
-        final TextView textView = binding.textNotifications;
-        profileViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+
+        // on below line creating a child fragment
+        Fragment childFragment = new SettingsFragment();
+
+        // on below line creating a fragment transaction and initializing it.
+        FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
+
+        // on below line replacing the fragment in child container with child fragment.
+        transaction.replace(R.id.childFragmentContainer, childFragment).commit();
+
         return root;
     }
 
@@ -52,12 +65,49 @@ public class ProfileFragment extends Fragment implements View.OnClickListener {
     @Override
     public void onClick(View view) {
         if (view == settingsBtn){
+            // on below line creating a child fragment
+            Fragment childFragment = new SettingsFragment();
+
+            // on below line creating a fragment transaction and initializing it.
+            FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
+
+            // on below line replacing the fragment in child container with child fragment.
+            transaction.replace(R.id.childFragmentContainer, childFragment).commit();
+
             updateUiForSubMenuShift(settingsBtn);
+
         }else if (view == notesBtn){
+            // on below line creating a child fragment
+            Fragment childFragment = new NotesFragment();
+
+            // on below line creating a fragment transaction and initializing it.
+            FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
+
+            // on below line replacing the fragment in child container with child fragment.
+            transaction.replace(R.id.childFragmentContainer, childFragment).commit();
+
             updateUiForSubMenuShift(notesBtn);
         }else if (view == workbookBtn){
+            // on below line creating a child fragment
+            Fragment childFragment = new WorkbookFragment();
+
+            // on below line creating a fragment transaction and initializing it.
+            FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
+
+            // on below line replacing the fragment in child container with child fragment.
+            transaction.replace(R.id.childFragmentContainer, childFragment).commit();
+
             updateUiForSubMenuShift(workbookBtn);
         }else if (view == statisticsBtn){
+            // on below line creating a child fragment
+            Fragment childFragment = new StatisticsFragment();
+
+            // on below line creating a fragment transaction and initializing it.
+            FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
+
+            // on below line replacing the fragment in child container with child fragment.
+            transaction.replace(R.id.childFragmentContainer, childFragment).commit();
+
             updateUiForSubMenuShift(statisticsBtn);
         }
     }
