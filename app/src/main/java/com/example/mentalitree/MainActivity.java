282697a916e5acdde9975b517b47c4238c935e9b
Maria <s185108@student.dtu.dk>
2024-06-21 13:37:44 +0200

    Removed obsolete code related to ActionBar from Mainactivity

--- app/src/main/java/com/example/mentalitree/MainActivity.java
+++ app/src/main/java/com/example/mentalitree/MainActivity.java
@@ -23,12 +23,6 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_today, R.id.navigation_motivation, R.id.navigation_profile)
-                .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
