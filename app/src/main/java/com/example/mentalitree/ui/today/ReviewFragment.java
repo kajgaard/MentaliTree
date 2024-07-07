2902f0a289420ab7a9fa1f02453eb4415ca0aca1
Maria <s185108@student.dtu.dk>
2024-07-07 16:03:04 +0200

    Creates the ReviewFragment with UI

---
+++ app/src/main/java/com/example/mentalitree/ui/today/ReviewFragment.java
@@ -0,0 +1,39 @@
+package com.example.mentalitree.ui.today;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.mentalitree.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ReviewFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ReviewFragment extends Fragment {
+
+
+
+    public ReviewFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_review, container, false);
+    }
+}
