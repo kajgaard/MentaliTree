8bb9a931b2d11c395ee322dfcdec8675ac8d4253
Maria <s185108@student.dtu.dk>
2024-07-08 18:22:17 +0200

    Added input validation on create user - No database document creation yet

--- app/src/main/java/com/example/mentalitree/Onboarding5.java
+++ app/src/main/java/com/example/mentalitree/Onboarding5.java
@@ -1,10 +1,14 @@
 package com.example.mentalitree;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,7 +21,9 @@ public class Onboarding5 extends AppCompatActivity implements View.OnClickListen
     ImageView avatar1;
     ImageView avatar2;
     ImageView avatar3;
+    EditText userIdEt, userPinEt, confirmPinEt;
     int avatarSelect = 1;
+    SharedPreferences sharedPreferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -31,6 +37,10 @@ public class Onboarding5 extends AppCompatActivity implements View.OnClickListen
         avatar2.setOnClickListener(this);
         avatar3 = findViewById(R.id.BunnyIv);
         avatar3.setOnClickListener(this);
+        userIdEt = findViewById(R.id.userIdEt);
+        userPinEt = findViewById(R.id.pincodeEt);
+        confirmPinEt = findViewById(R.id.confirm_pincodeEt);
+
     }
 
     @Override
@@ -52,8 +62,43 @@ public class Onboarding5 extends AppCompatActivity implements View.OnClickListen
             avatarSelect = 3;
         }
         if(view == nextBtn){
-            Intent intent = new Intent(this, LogInActivity.class);
-            startActivity(intent);
+
+            if(areCredentialsOkay()){
+                Intent intent = new Intent(this, LogInActivity.class);
+                startActivity(intent);
+            }else{
+                Toast.makeText(this, "Please make sure your information is valid", Toast.LENGTH_LONG).show();
+            }
+
         }
+    }
+
+    public boolean areCredentialsOkay(){
+        boolean flag = true;
+        if (userIdEt.getText().toString().isEmpty()){
+            userIdEt.setError("Cannot be empty");
+            flag = false;
+        }
+        if (userPinEt.getText().toString().isEmpty()) {
+            userPinEt.setError("Cannot be empty");
+            flag = false;
+        }
+        if (confirmPinEt.getText().toString().isEmpty()) {
+            confirmPinEt.setError("Cannot be empty");
+            flag = false;
+        }
+        if (!userPinEt.getText().toString().equals(confirmPinEt.getText().toString())) {
+            confirmPinEt.setError("Not equal to pincode above");
+            flag = false;
+        }
+
+        return flag;
+    }
+    public void setHasUserBoolInSharedPrefs(){
+        sharedPreferences = getSharedPreferences("mUserPrefs", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor;
+        editor = sharedPreferences.edit();
+        editor.putBoolean("has_user", true);
+        editor.apply();
     }
 }
