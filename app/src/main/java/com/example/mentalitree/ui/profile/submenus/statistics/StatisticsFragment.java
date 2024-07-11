28ecde7e42c8163a29c40c33584809274228a6ff
Maria <s185108@student.dtu.dk>
2024-07-11 18:07:47 +0200

    Stats view streaks are now retrieved from datahandler

--- app/src/main/java/com/example/mentalitree/ui/profile/submenus/statistics/StatisticsFragment.java
+++ app/src/main/java/com/example/mentalitree/ui/profile/submenus/statistics/StatisticsFragment.java
@@ -7,60 +7,49 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.example.mentalitree.DataHandler;
 import com.example.mentalitree.R;
+import com.example.mentalitree.databinding.BarchartStatWidgetBinding;
+import com.example.mentalitree.databinding.CurrentStatWidgetBinding;
+import com.example.mentalitree.databinding.FragmentProfileStatisticsBinding;
+import com.example.mentalitree.databinding.LongestStatWidgetBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link StatisticsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class StatisticsFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+    FragmentProfileStatisticsBinding binding;
+    BarchartStatWidgetBinding barchartStatWidgetBinding;
+    LongestStatWidgetBinding longestStatWidgetBinding;
+    CurrentStatWidgetBinding currentStatWidgetBinding;
+    TextView currentStreakTv, totalEffortStreakTv;
+    DataHandler dataHandler = DataHandler.getInstance();
     public StatisticsFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment StatisticsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static StatisticsFragment newInstance(String param1, String param2) {
-        StatisticsFragment fragment = new StatisticsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile_statistics, container, false);
+
+        binding = FragmentProfileStatisticsBinding.inflate(inflater, container, false);
+
+        View root = binding.getRoot();
+        totalEffortStreakTv = root.findViewById(R.id.longestNumberTv);
+        totalEffortStreakTv.setText(""+dataHandler.getTotalEffortStreak());
+
+        currentStreakTv = root.findViewById(R.id.currentNumberTv);
+        currentStreakTv.setText(""+dataHandler.getUsersCurrentStreak());
+
+
+        return root;
     }
 }
